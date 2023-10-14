1e19942816a6e77ea3c588318f8f21ef1951e965
zacwong2151 <zacwong2151@gmail.com>
2023-10-14 22:41:18 +0800

    Delete address field from volunteer
    Following the AB3 guide, users currently have an 'address' field which
    stores their home location
    
    This makes sense for an AddressBook, but as iVolunteer primarly
    manages volunteers and events, there is no need to store the address of
    each volunteer
    
    Let's remove the address field of volunteers
    
    In the next few commits, let's change the 'tag' field of each volunteer
    to 'skills'

--- src/main/java/seedu/address/logic/parser/VolunteerEditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/VolunteerEditCommandParser.java
@@ -2,7 +2,6 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
@@ -32,7 +31,7 @@ public class VolunteerEditCommandParser implements Parser<VolunteerEditCommand> 
     public VolunteerEditCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_TAG);
 
         Index index;
 
@@ -43,7 +42,7 @@ public class VolunteerEditCommandParser implements Parser<VolunteerEditCommand> 
                                                     VolunteerEditCommand.MESSAGE_USAGE), pe);
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL);
 
         EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
 
@@ -55,9 +54,6 @@ public class VolunteerEditCommandParser implements Parser<VolunteerEditCommand> 
         }
         if (argMultimap.getValue(PREFIX_EMAIL).isPresent()) {
             editPersonDescriptor.setEmail(ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get()));
-        }
-        if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
-            editPersonDescriptor.setAddress(ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get()));
         }
         parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editPersonDescriptor::setTags);
 
