7aa0dfe8740f4ab11725080c8aea86c99dfba4f0
brendan <brenstack62@gmail.com>
2023-10-18 03:04:38 +0800

    Fix import order for Checkstyle

--- src/test/java/seedu/address/model/event/DateTimeTest.java
+++ src/test/java/seedu/address/model/event/DateTimeTest.java
@@ -1,7 +1,7 @@
 package seedu.address.model.event;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
