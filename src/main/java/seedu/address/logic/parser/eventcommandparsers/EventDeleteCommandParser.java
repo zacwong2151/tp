0aa4429c449f580cbae11fbd48307114d01874a4
tiongMax <maxtiongcs16@gmail.com>
2023-10-18 19:16:54 +0800

    Add event delete command
    User can now delete the event created.

---
+++ src/main/java/seedu/address/logic/parser/eventcommandparsers/EventDeleteCommandParser.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.parser.eventcommandparsers;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.eventcommands.EventDeleteCommand;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new EventDeleteCommand object
+ */
+public class EventDeleteCommandParser implements Parser<EventDeleteCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the EventDeleteCommand
+     * and returns a EventDeleteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public EventDeleteCommand parse(String args) throws ParseException {
+        try {
+            System.out.println("args: " + args);
+            Index index = ParserUtil.parseIndex(args);
+            System.out.println("index: " + index);
+            return new EventDeleteCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, EventDeleteCommand.MESSAGE_USAGE), pe);
+        }
+    }
+
+}
