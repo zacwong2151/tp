3d42a429b87561f82efee73af1f27d14602651db
ruo-x <e0970274@u.nus.edu>
2023-10-18 17:02:40 +0800

    checkstyle changes

--- src/main/java/seedu/address/logic/commands/eventcommands/EventListCommand.java
+++ src/main/java/seedu/address/logic/commands/eventcommands/EventListCommand.java
@@ -1,12 +1,15 @@
 package seedu.address.logic.commands.eventcommands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EVENTS;
+
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EVENTS;
-
+/**
+ * Shows a list of all events.
+ */
 public class EventListCommand extends Command {
     public static final String COMMAND_WORD = "elist";
 
