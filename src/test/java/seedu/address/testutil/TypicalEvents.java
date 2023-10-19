deb658e61f514b97429f9b5974eb4a6bafc236d8
jordankanghm <jordankanghm@gmail.com>
2023-10-19 10:07:27 +0800

    Create EventCommandCreate Integration test cases

--- src/test/java/seedu/address/testutil/TypicalEvents.java
+++ src/test/java/seedu/address/testutil/TypicalEvents.java
@@ -1,6 +1,7 @@
 package seedu.address.testutil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import seedu.address.model.EventStorage;
@@ -10,6 +11,61 @@ import seedu.address.model.event.Event;
  * A utility class containing a list of {@code Volunteer} objects to be used in tests.
  */
 public class TypicalEvents {
+    public static final Event FIRST = new EventBuilder().withEventName("Clean 1")
+            .withRoles("cleaner")
+            .withDateAndTime("23/10/2023 1800")
+            .withLocation("Sembawang")
+            .withDescription("Clean up la")
+            .withMaterials("trash bag", "gloves")
+            .withBudget("50.00").build();
+
+    public static final Event SECOND = new EventBuilder().withEventName("Clean 2")
+            .withRoles("cleaner")
+            .withDateAndTime("23/10/2023 1800")
+            .withLocation("Sembawang")
+            .withDescription("Clean up la")
+            .withMaterials("trash bag", "gloves")
+            .withBudget("50.00").build();
+
+    public static final Event THIRD = new EventBuilder().withEventName("Clean 3")
+            .withRoles("cleaner")
+            .withDateAndTime("23/10/2023 1800")
+            .withLocation("Sembawang")
+            .withDescription("Clean up la")
+            .withMaterials("trash bag", "gloves")
+            .withBudget("50.00").build();
+
+    public static final Event FOURTH = new EventBuilder().withEventName("Clean 4")
+            .withRoles("cleaner")
+            .withDateAndTime("23/10/2023 1800")
+            .withLocation("Sembawang")
+            .withDescription("Clean up la")
+            .withMaterials("trash bag", "gloves")
+            .withBudget("50.00").build();
+
+    public static final Event FIFTH = new EventBuilder().withEventName("Clean 5")
+            .withRoles("cleaner")
+            .withDateAndTime("23/10/2023 1800")
+            .withLocation("Sembawang")
+            .withDescription("Clean up la")
+            .withMaterials("trash bag", "gloves")
+            .withBudget("50.00").build();
+
+    public static final Event SIXTH = new EventBuilder().withEventName("Clean 6")
+            .withRoles("cleaner")
+            .withDateAndTime("23/10/2023 1800")
+            .withLocation("Sembawang")
+            .withDescription("Clean up la")
+            .withMaterials("trash bag", "gloves")
+            .withBudget("50.00").build();
+
+    public static final Event SEVENTH = new EventBuilder().withEventName("Clean 7")
+            .withRoles("cleaner")
+            .withDateAndTime("23/10/2023 1800")
+            .withLocation("Sembawang")
+            .withDescription("Clean up la")
+            .withMaterials("trash bag", "gloves")
+            .withBudget("50.00").build();
     private TypicalEvents() {} // prevents instantiation
 
     /**
@@ -24,6 +80,7 @@ public class TypicalEvents {
     }
 
     public static List<Event> getTypicalEvents() {
-        return new ArrayList<>();
+
+        return new ArrayList<>(Arrays.asList(FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH));
     }
 }
