853863889e68051b495fbf483db85a7cc8c72275
brendan <brenstack62@gmail.com>
2023-10-30 23:57:52 +0800

    Implement Role quantity fields

--- src/main/java/seedu/address/model/event/Role.java
+++ src/main/java/seedu/address/model/event/Role.java
@@ -9,33 +9,155 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Role {
 
-    public static final String MESSAGE_CONSTRAINTS = "Roles should only contain alphanumeric characters and spaces, "
-                                                        + "and it should not be blank\"";
+    public static final String MESSAGE_CONSTRAINTS = "Role should have the format [role quantity] [role name], "
+            + "role quantity should be a whole number (0 or positive), and it should not be blank";
+
     /*
-     * The first character of the name must not be a whitespace,
+     * The first character of the material must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      * The expression allows for multiple words to be inputted.
+     * The expression matches a valid integer, followed by the words to be inputted.
      */
-    public static final String VALIDATION_REGEX = "[A-Za-z0-9 _.,!\"'/$]*";
+    public static final String VALIDATION_REGEX_UI = "[0-9]+\\s/\\s[0-9]+\\s[\\p{Alnum}][\\p{Alnum} ]*";
+
+    /*
+     * The first character of the material must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     * The expression allows for multiple words to be inputted.
+     * The expression matches a valid integer, followed by the words to be inputted.
+     */
+    public static final String VALIDATION_REGEX = "[0-9]+\\s[\\p{Alnum}][\\p{Alnum} ]*";
+
+    /*
+     * The first character of the material must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     * The expression allows for multiple words to be inputted.
+     * This regex applies only for the material name portion of the material string.
+     */
+    public static final String VALIDATION_REGEX_ROLE_NAME = "[\\p{Alnum}][\\p{Alnum} ]*";
 
     public final String roleName;
 
+    public final int currentQuantity;
+
+    public final int requiredQuantity;
+
     /**
-     * Constructs a {@code Role}.
+     * Constructs a {@code Role} with a specified quantity. Current quantity is set to 0. The required
+     * quantity should be included in the {@code role} parameter.
+     *
+     * @param role A valid role containing a valid whole number quantity at the front.
+     */
+    public Role(String role) {
+        requireNonNull(role);
+        checkArgument(isValidRole(role), MESSAGE_CONSTRAINTS);
+        this.roleName = role.substring(role.indexOf(" ") + 1);
+        this.currentQuantity = 0;
+        this.requiredQuantity = Integer.parseInt(role.substring(0, role.indexOf(" ")));
+    }
+
+    /**
+     * Constructs a {@code Role} with a specified quantity. Current quantity is set to 0.
      *
      * @param roleName A valid role name.
+     * @param requiredQuantity A valid integer quantity (0 or positive integer).
      */
-    public Role(String roleName) {
+    public Role(String roleName, int requiredQuantity) {
         requireNonNull(roleName);
         checkArgument(isValidRoleName(roleName), MESSAGE_CONSTRAINTS);
+        checkArgument(isValidQuantity(requiredQuantity), MESSAGE_CONSTRAINTS);
         this.roleName = roleName;
+        this.currentQuantity = 0;
+        this.requiredQuantity = requiredQuantity;
+    }
+
+    /**
+     * Constructs a {@code Role} with a specified current and required quantity.
+     *
+     * @param roleName A valid role name.
+     * @param currentQuantity A valid integer current quantity (0 or positive integer).
+     * @param requiredQuantity A valid integer required quantity (0 or positive integer).
+     */
+    public Role(String roleName, int currentQuantity, int requiredQuantity) {
+        requireNonNull(roleName);
+        checkArgument(isValidRoleName(roleName), MESSAGE_CONSTRAINTS);
+        checkArgument(isValidQuantity(currentQuantity), MESSAGE_CONSTRAINTS);
+        checkArgument(isValidQuantity(requiredQuantity), MESSAGE_CONSTRAINTS);
+        this.roleName = roleName;
+        this.currentQuantity = currentQuantity;
+        this.requiredQuantity = requiredQuantity;
+    }
+
+    /**
+     * Constructs a {@code Role} from a {@code String role} in 2 formats: [quantity] [role name] or
+     * [quantity] / [quantity] [role name] (UI string format). This allows the generation of a Role with a current
+     * quantity field from a string.
+     * @param role A valid role containing 1 or 2 valid whole number quantities in the format: [quantity] [role name]
+     *                 or [quantity] / [quantity] [role name]
+     * @return The material generated from the material string.
+     * @throws IllegalArgumentException If the material cannot be parsed from its string or UI string representation.
+     */
+    public static Role fromUiString(String role) {
+        requireNonNull(role);
+
+        // of the format [quantity] [material name]
+        if (isValidRole(role)) {
+            return new Role(role);
+        } else {
+            checkArgument(isValidUiString(role), MESSAGE_CONSTRAINTS);
+            int currentQuantity = Integer.parseInt(role.substring(0, role.indexOf(" / ")));
+            String remainingStr = role.substring(role.indexOf(" / ") + 3);
+
+            int requiredQuantity = Integer.parseInt(remainingStr.substring(0, remainingStr.indexOf(" ")));
+            String roleName = remainingStr.substring(remainingStr.indexOf(" ") + 1);
+
+            return new Role(roleName, currentQuantity, requiredQuantity);
+        }
+    }
+
+    /**
+     * Returns true if a given string is a valid role (of the format [quantity] [role name]).
+     */
+    public static boolean isValidRole(String test) {
+        try {
+            int quantity = Integer.parseInt(test.split("\\s+")[0]);
+            return test.matches(VALIDATION_REGEX) && isValidQuantity(quantity);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            // if the role cannot be parsed into format [number] [role name]
+            // or the number is not valid
+            return false;
+        }
+    }
+
+    /**
+     * Returns true if a given string is a valid role (of the format [quantity] / [quantity] [role name]).
+     */
+    public static boolean isValidUiString(String test) {
+        try {
+            int currentQuantity = Integer.parseInt(test.split("\\s+")[0]);
+            int requiredQuantity = Integer.parseInt(test.split("\\s+")[2]);
+            return test.matches(VALIDATION_REGEX_UI)
+                    && isValidQuantity(currentQuantity)
+                    && isValidQuantity(requiredQuantity);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            // if the material cannot be parsed into format [number] / [number] [material name]
+            // or the number is not valid
+            return false;
+        }
     }
 
     /**
      * Returns true if a given string is a valid role name.
      */
     public static boolean isValidRoleName(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return test.matches(VALIDATION_REGEX_ROLE_NAME);
+    }
+
+    /**
+     * Returns true if a given integer is a valid quantity.
+     */
+    public static boolean isValidQuantity(int test) {
+        return (test >= 0);
     }
 
     @Override
@@ -50,7 +172,9 @@ public class Role {
         }
 
         Role otherRole = (Role) other;
-        return roleName.equals(otherRole.roleName);
+        return roleName.equals(otherRole.roleName)
+                && currentQuantity == otherRole.currentQuantity
+                && requiredQuantity == otherRole.requiredQuantity;
     }
 
     @Override
@@ -61,8 +185,24 @@ public class Role {
     /**
      * Format state as text for viewing.
      */
+    @Override
     public String toString() {
-        return '[' + roleName + ']';
+        return currentQuantity + " / " + requiredQuantity + " " + roleName;
     }
 
+    /**
+     * Adds 1 to the current quantity, and returns a new Role with the new quantity.
+     * @return A new Role with the updated quantity.
+     */
+    public Role addRoleManpower() {
+        return new Role(roleName, currentQuantity + 1, requiredQuantity);
+    }
+
+    public boolean hasEnoughManpower() {
+        return currentQuantity >= requiredQuantity;
+    }
+
+    public String toUiString() {
+        return currentQuantity + " / " + requiredQuantity + " " + roleName;
+    }
 }
--- src/main/java/seedu/address/storage/event/JsonAdaptedRole.java
+++ src/main/java/seedu/address/storage/event/JsonAdaptedRole.java
@@ -1,7 +1,7 @@
 package seedu.address.storage.event;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.event.Role;
@@ -13,12 +13,29 @@ public class JsonAdaptedRole {
 
     private final String roleName;
 
+    private final String currentQuantity;
+
+    private final String requiredQuantity;
+
     /**
-     * Constructs a {@code JsonAdaptedRole} with the given {@code roleName}.
+     * Constructs a {@code JsonAdaptedRole} with the given {@code roleName}, disregarding quantity.
      */
-    @JsonCreator
     public JsonAdaptedRole(String roleName) {
         this.roleName = roleName;
+        this.currentQuantity = "0";
+        this.requiredQuantity = "0";
+    }
+
+    /**
+     * Constructs a {@code JsonAdaptedRole} with the given {@code role}.
+     */
+    @JsonCreator
+    public JsonAdaptedRole(@JsonProperty("roleName") String roleName,
+                           @JsonProperty("currentQuantity") String currentQuantity,
+                           @JsonProperty("requiredQuantity") String requiredQuantity) {
+        this.roleName = roleName;
+        this.currentQuantity = currentQuantity;
+        this.requiredQuantity = requiredQuantity;
     }
 
     /**
@@ -26,9 +43,10 @@ public class JsonAdaptedRole {
      */
     public JsonAdaptedRole(Role source) {
         roleName = source.roleName;
+        currentQuantity = String.valueOf(source.currentQuantity);
+        requiredQuantity = String.valueOf(source.requiredQuantity);
     }
 
-    @JsonValue
     public String getRoleName() {
         return roleName;
     }
@@ -39,10 +57,26 @@ public class JsonAdaptedRole {
      * @throws IllegalValueException if there were any data constraints violated in the adapted role.
      */
     public Role toModelType() throws IllegalValueException {
-        if (!Role.isValidRoleName(roleName)) {
+        int currentQuantityInt;
+        int requiredQuantityInt;
+
+        try {
+            currentQuantityInt = Integer.parseInt(currentQuantity);
+            requiredQuantityInt = Integer.parseInt(requiredQuantity);
+        } catch (NumberFormatException e) {
             throw new IllegalValueException(Role.MESSAGE_CONSTRAINTS);
         }
-        return new Role(roleName);
+
+        // validation for role name and quantity values
+        boolean isValidRole = (Role.isValidRoleName(roleName)
+                && Role.isValidQuantity(currentQuantityInt)
+                && Role.isValidQuantity(requiredQuantityInt));
+
+        if (!isValidRole) {
+            throw new IllegalValueException(Role.MESSAGE_CONSTRAINTS);
+        }
+
+        return new Role(roleName, currentQuantityInt, requiredQuantityInt);
     }
 
 }
