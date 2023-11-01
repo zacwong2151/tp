1c6c26b590815c17f34feb13bd475653aadeb560
ruo-x <e0970274@u.nus.edu>
2023-11-01 17:57:31 +0800

    Resolve CI erros

--- src/test/java/seedu/address/logic/commands/EventListVolunteerCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EventListVolunteerCommandTest.java
@@ -1,6 +1,9 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalEvents.getTypicalEventStorage;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
