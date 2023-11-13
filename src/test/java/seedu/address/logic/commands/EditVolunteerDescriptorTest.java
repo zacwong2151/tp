c15c11530ad7b94ca33d218a4f9537a800b69d60
Brendan Koh <brenstack62@gmail.com>
2023-11-14 02:08:00 +0800

    Remove functionality to edit volunteer name

--- src/test/java/seedu/address/logic/commands/EditVolunteerDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditVolunteerDescriptorTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_SKILL_HUSBAND;
 
@@ -35,13 +34,9 @@ public class EditVolunteerDescriptorTest {
         // different values -> returns false
         assertFalse(DESC_AMY.equals(DESC_BOB));
 
-        // different name -> returns false
-        EditVolunteerDescriptor editedAmy = new EditVolunteerDescriptorBuilder(DESC_AMY).withName(VALID_NAME_BOB)
-                                            .build();
-        assertFalse(DESC_AMY.equals(editedAmy));
-
         // different phone -> returns false
-        editedAmy = new EditVolunteerDescriptorBuilder(DESC_AMY).withPhone(VALID_PHONE_BOB).build();
+        EditVolunteerDescriptor editedAmy = new EditVolunteerDescriptorBuilder(DESC_AMY)
+                .withPhone(VALID_PHONE_BOB).build();
         assertFalse(DESC_AMY.equals(editedAmy));
 
         // different email -> returns false
@@ -56,8 +51,7 @@ public class EditVolunteerDescriptorTest {
     @Test
     public void toStringMethod() {
         EditVolunteerDescriptor editVolunteerDescriptor = new EditVolunteerDescriptor();
-        String expected = EditVolunteerDescriptor.class.getCanonicalName() + "{name="
-                + editVolunteerDescriptor.getName().orElse(null) + ", phone="
+        String expected = EditVolunteerDescriptor.class.getCanonicalName() + "{phone="
                 + editVolunteerDescriptor.getPhone().orElse(null) + ", email="
                 + editVolunteerDescriptor.getEmail().orElse(null) + ", skills="
                 + editVolunteerDescriptor.getSkills().orElse(null) + ", assigned events="
